fda21569df6fb6afe13fa88693c953d69351d4be
Shagitora <rvishkazflex@gmail.com>
2025-05-27 00:52:01 +0300

    reset gitignore

--- app/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.config import settings
-
-engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
